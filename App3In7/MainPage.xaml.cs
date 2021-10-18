9a897f6ff40c7785e2c92a1085545a11e1c2a3c9
Renato Toasa <renato_toasa@hotmail.com>
2021-10-18 18:59:17 -0500

    Add project files.

---
+++ App3In7/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3In7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            //almacenar en variable, el contenido de las cajas de texto
+            string usuario = txtUsuario.Text;
+            string clave = txtClave.Text;
+           await DisplayAlert("mensaje", usuario, "ok");
+             //Abrir pantalla dos - envio de datos entre pantallas
+            await Navigation.PushAsync(new viewDos(usuario, clave)); //rtoasa   12345
+        }
+    }
+}
---
+++ App3In7/viewDos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3In7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewDos : ContentPage
+    {
+        //usuario = rtoasa   clave = 12345
+        public viewDos(string usuario, string clave)
+        {
+            InitializeComponent();
+            //visualizar datos
+            lblUsuario.Text = usuario; //rtoasa
+            lblClave.Text = clave; //12345
+
+            
+        }
+    }
+}
